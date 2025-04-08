3f79c067d9fc081776ed55d68996257bf695231f
samhitmantrala8 <128477709+samhitmantrala8@users.noreply.github.com>
2025-04-08 21:54:53 +0530

    final backend

--- backend/app.py
+++ backend/app.py
@@ -79,6 +79,9 @@ def recommend():
     results = [metadata[i] for i in I[0] if i < len(metadata)]
     return jsonify(results)
 
+
+port = int(os.environ.get("PORT", 10000))
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host="0.0.0.0", port=port)
 
