a81641272e8d8efb4f802aede31643d96473ff43
samhitmantrala8 <128477709+samhitmantrala8@users.noreply.github.com>
2025-04-08 21:50:13 +0530

    correcteed the port

--- backend/app.py
+++ backend/app.py
@@ -80,5 +80,5 @@ def recommend():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=10000)
 
